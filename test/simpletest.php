9ed13532ab58504295f7ebb6f2b03de031b62629
BOUILLANT Anthony <Anthony.BOUILLANT@cpnv.ch>
2021-09-14 16:06:51 +0200

    selectOne & Update

--- test/simpletest.php
+++ test/simpletest.php
@@ -1,27 +1,57 @@
 <?php
 
-/**
- * File : simpletest.php
- * Author : X. Carrel
- * Created : 14.09.21
- * Modified last :
- **/
+use PHPUnit\Framework\TestCase;
 
-require "../model/DB.php";
-echo "\n>>>>> Test selectMany:\n";
-$res = DB::selectMany("SELECT * FROM roles", []);
-var_dump($res);
+require_once("./model/DB.php");
 
-echo "\n>>>>> Test selectOne:\n";
-$res = DB::selectOne("SELECT * FROM roles where slug = :slug", ["slug" => "MOD"]);
-var_dump($res);
+class simpletest extends TestCase
+{
+    public function testUpdate()
+    {
+        $res  = DB::execute("UPDATE roles set name = :name WHERE slug = :slug", ["slug" => "XXX", "name" => "Correcteur"]);
+        $this->assertTrue($res);
+    }
 
-// echo "\n>>>>> Test insert:\n";
-// $res = DB::insert("INSERT INTO roles(slug,name) VALUES (:slug, :name)", ["slug" => "XXX", "name" => "Slasher"]);
-// var_dump($res);
+    public function testSelectOne()
+    {
+        $res = DB::selectOne("SELECT * FROM roles where slug = :slug", ["slug" => "MOD"]);
 
-echo "\n>>>>> Test update:\n";
-$res = DB::execute("UPDATE roles set name = :name WHERE slug = :slug", ["slug" => "XXX", "name" => "Correcteur"]);
-var_dump($res);
+        $slugValue = $res[0]['slug'];
 
-echo "\nDone\n";
+        $this->assertSame($slugValue, 'MOD');
+    }
+
+    // public function 
+
+    // public function test()
+    // {
+
+    //     /**
+    //      * File : simpletest.php
+    //      * Author : X. Carrel
+    //      * Created : 14.09.21
+    //      * Modified last :
+    //      **/
+
+
+    //     // echo "\n>>>>> Test selectMany:\n";
+    //     // $res = DB::selectMany("SELECT * FROM roles", []);
+    //     // var_dump($res);
+
+    //     // echo "\n>>>>> Test selectOne:\n";
+    //     // $res = DB::selectOne("SELECT * FROM roles where slug = :slug", ["slug" => "MOD"]);
+    //     // var_dump($res);
+
+    //     // // echo "\n>>>>> Test insert:\n";
+    //     // // $res = DB::insert("INSERT INTO roles(slug,name) VALUES (:slug, :name)", ["slug" => "XXX", "name" => "Slasher"]);
+    //     // // var_dump($res);
+
+    //     // echo "\n>>>>> Test update:\n";
+    //     // $res = DB::execute("UPDATE roles set name = :name WHERE slug = :slug", ["slug" => "XXX", "name" => "Correcteur"]);
+    //     // var_dump($res);
+
+    //     // echo "\nDone\n";
+
+    //     // $this->assert
+    // }
+}
