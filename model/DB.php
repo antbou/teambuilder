e65998b738e464772779eb57449603c25c8e2bae
BOUILLANT Anthony <Anthony.BOUILLANT@cpnv.ch>
2021-09-14 15:12:21 +0200

    test manuel

---
+++ model/DB.php
@@ -0,0 +1,50 @@
+<?php
+
+class DB
+{
+
+    public static function getPdo(): PDO
+    {
+        require_once('.env.php');
+        return new PDO('mysql:host=' . DBHOST . ';dbname=' . DBNAME . ';charset=utf8', DBUSERNAME, DBPASSWORD, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public static function selectMany(string $query, array $params): array
+    {
+        $sth = self::getPdo()->prepare($query);
+        $sth->execute($params);
+
+        $result = $sth->fetchAll();
+        return $result;
+    }
+
+    public static function selectOne(string $query, array $params): array
+    {
+        $sth = self::getPdo()->prepare($query);
+        $sth->execute($params);
+
+        $result = $sth->fetchAll();
+        return $result;
+    }
+
+    public static function insert(string $query, array $params): int
+    {
+        $db = self::getPdo();
+        $sth = $db->prepare($query);
+        $sth->execute($params);
+
+        $result = $db->lastInsertId();
+        return $result;
+    }
+
+    public static function execute(string $query, array $params)
+    {
+        $db = self::getPdo();
+        $sth = $db->prepare($query);
+        $sth->execute($params);
+
+        return $sth->execute($params);;
+    }
+}
