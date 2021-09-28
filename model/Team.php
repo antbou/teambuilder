fe50652884b7ff09a3c6cc53944a56d1987850c2
BOUILLANT Anthony <Anthony.BOUILLANT@cpnv.ch>
2021-09-28 14:20:13 +0200

    Team tests ok

--- model/Team.php
+++ model/Team.php
@@ -4,7 +4,7 @@ require_once('DB.php');
 
 class Team
 {
-    public $id;
+    public $id = null;
     public $name;
     public $state_id;
 
@@ -13,9 +13,17 @@ class Team
      *
      * @return void
      */
-    public function create()
+    public function create(): bool
     {
+        $check = DB::selectOne("SELECT * FROM teams WHERE NAME = :name", ['name' => $this->name]);
+
+        if (!empty($check)) {
+            return false;
+        }
+
         $this->id = DB::insert("INSERT INTO teams(name,state_id) VALUES (:name, :state_id)", ['name' => $this->name, 'state_id' => $this->state_id]);
+
+        return true;
     }
 
     /**
@@ -26,12 +34,16 @@ class Team
      * @param integer $state_id
      * @return Team|null
      */
-    public static function make(int $id = null, string $name, int $state_id): Team
+    public static function make(array $params): Team
     {
         $team = new Team();
-        $team->id = $id;
-        $team->name = $name;
-        $team->state_id = $state_id;
+
+        if (isset($params['id'])) {
+            $team->id = $params['id'];
+        }
+
+        $team->name = $params['name'];
+        $team->state_id = $params['state_id'];
 
         return $team;
     }
@@ -40,32 +52,45 @@ class Team
     {
         $res = DB::selectOne("SELECT * FROM teams where id = :id", ['id' => $id]);
 
+        // Si il n'y a rien, return null
         if (!isset($res[0])) {
             return null;
         }
 
         $res = $res[0];
-
-        return self::make($res['id'], $res['name'], $res['state_id']);
+        return self::make(['id' => $res['id'], 'name' => $res['name'], 'state_id' => $res['state_id']]);
     }
 
     public static function all(): array
     {
-        return [];
+        return DB::selectMany("SELECT * FROM teams", []);
     }
 
     public function save(): bool
     {
-        return true;
+
+        $check = DB::selectOne("SELECT * FROM teams WHERE NAME = :name", ['name' => $this->name]);
+
+        // si il n'est pas vide, alors return false, car le nom sera dupliqué
+        if (!empty($check)) {
+            return false;
+        }
+
+        return DB::execute("UPDATE teams set name = :name, state_id = :state_id WHERE id = :id", ['id' => $this->id, 'name' => $this->name, 'state_id' => $this->state_id]);
     }
 
     public function delete(): bool
     {
-        return true;
+        return self::destroy($this->id);
     }
 
     public static function destroy(int $id): bool
     {
-        return true;
+        try {
+            DB::execute("DELETE FROM teams WHERE id = :id", ['id' => $id]);
+            return true;
+        } catch (\Throwable $th) {
+            return false;
+        }
     }
 }
