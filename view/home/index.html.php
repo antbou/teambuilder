25a86f528cb2531ad110a53ebe43289cefb33ae3
BOUILLANT Anthony <Anthony.BOUILLANT@cpnv.ch>
2021-10-05 15:07:57 +0200

    View homepage

--- view/home/index.html.php
+++ view/home/index.html.php
@@ -1,1 +1,6 @@
-<?= $_SESSION['membre']->name ?>
+<div class="container-fluid bg-success ">
+    <div class="text-center">
+        <h1>Teambuilder</h1>
+        <p><?= $_SESSION['membre']->name ?></p>
+    </div>
+</div>
