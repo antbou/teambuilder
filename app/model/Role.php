20e69b9f2f3fb33982f287029fef383caa988924
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-11-10 13:41:11 +0100

    Refactoring Role class

--- app/model/Role.php
+++ app/model/Role.php
@@ -22,15 +22,6 @@ class Role extends Model
         return $role;
     }
 
-    public function create(): bool // create db record from object
-    {
-        try {
-            return DB::insert("INSERT INTO roles(slug,name) VALUES (:slug, :name)", ["slug" => $this->slug, "name" => $this->name]);
-        } catch (\PDOException $Exception) {
-            return false;
-        }
-    }
-
     static function find($id): ?Role
     {
         $db = DB::selectOne("SELECT * FROM roles where id = :id", ["id" => "$id"]);
@@ -47,33 +38,6 @@ class Role extends Model
         }
 
         return $res;
-    }
-
-    public function save(): bool
-    {
-        try {
-            return DB::execute("UPDATE roles set name = :name, slug = :slug WHERE id = :id", ["slug" => $this->slug, "name" => $this->name, "id" => $this->id]);
-        } catch (\PDOException $Exception) {
-            return false;
-        }
-    }
-
-    public function delete(): bool
-    {
-        try {
-            return DB::execute("DELETE FROM roles WHERE id = :id", ["id" => $this->id]);
-        } catch (\PDOException $Exception) {
-            return false;
-        }
-    }
-
-    static function destroy($id): bool
-    {
-        try {
-            return DB::execute("DELETE FROM roles WHERE id = :id", ["id" => $id]);
-        } catch (\PDOException $Exception) {
-            return false;
-        }
     }
 
     public function members()
